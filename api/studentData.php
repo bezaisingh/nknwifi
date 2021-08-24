436368a511e8bc27b4e1863919e57afbd5b742ac
Bijay Singh <bijay611@gmail.com>
2021-08-25 01:07:33 +0530

    student data is being saved in database, but radio button data is not being saved for the time being. no api to save staff data is made.

---
+++ api/studentData.php
@@ -0,0 +1,104 @@
+<?php
+
+// turn on error reporting
+error_reporting(1);
+ini_set('error_reporting', E_ALL);
+//debug session
+//var_dump($_SESSION);
+
+
+include 'dbConfig.php';
+
+if(isset($_POST["submit"]))
+{
+
+$Enroll=$_POST["enroll"];
+
+$Fname=$_POST["fname"];
+
+$Lname=$_POST["lname"];
+
+$Dept=$_POST["dept"];
+
+$Email=$_POST["email"];
+
+$MobNo=$_POST["mobno"];
+
+$Course=$_POST["course"];
+
+$Semester=$_POST["semester"];
+
+$YearOfCompletion=$_POST["yearOfCompletion"];
+
+$HostelNo=$_POST["hostelNo"];
+
+    
+if(!empty($_POST['email'])){
+    
+        $sql="select * from student_user WHERE email='email'";
+        $sqli_run=mysqli_query($conn,$sql);
+    
+        if(mysqli_num_rows($sqli_run)>0)
+        {
+        echo"<script>alert('Email ID already exists')</script>";
+        }
+    
+       else {
+    
+
+        $sql="INSERT INTO student_user(
+                                         enrollNo,
+                                         firstName,
+                                         lastName, 
+                                    department,
+                                         email, 
+                                         mobNo, 
+                                         courseName, 
+                                         semester, 
+                                         yearOfCompletion, 
+                                         hostelNo                                      
+                                         ) 
+                                         
+                                         VALUES (
+                                                    '$Enroll',
+                                                    '$Fname',
+                                                    '$Lname',
+                                                    '$Dept', 
+                                                   
+                                                    
+                                                    '$Email',
+                                                    '$MobNo',
+                                                    
+                                                    '$Course',
+                                                    '$Semester',
+                                                    '$YearOfCompletion',
+                                                    '$HostelNo'
+                                                    )";
+            if($conn->query($sql)===TRUE)
+                {
+                    	  /*For popup window and to redirect to xxxx page*/
+                
+             echo   '<script type="text/javascript"> 
+             alert("Application Submited!!!"); 
+             window.location.href = "../index.html";
+             </script>';
+                    
+                }
+                else
+                {
+                    echo"Error:" . $sql . "<br>" . $conn->error;
+                }
+                   
+    }
+     
+} 
+   
+    mysqli_close($conn);
+   
+}
+else
+	{
+		echo "<script>alert('Password/Email Does not Match')</script>";
+    }
+   
+?>
---
+++ dbBackups/nknwifi.sql
@@ -0,0 +1,77 @@
+/*
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 8.0.21 : Database - nknwifi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`nknwifi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `nknwifi`;
+
+/*Table structure for table `staff_user` */
+
+DROP TABLE IF EXISTS `staff_user`;
+
+CREATE TABLE `staff_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `campus` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `enrollNo` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `firstName` varchar(30) DEFAULT NULL,
+  `lastName` varchar(30) DEFAULT NULL,
+  `fatherName` varchar(30) DEFAULT NULL,
+  `motherName` varchar(30) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `department` varchar(30) DEFAULT NULL,
+  `courseName` varchar(30) DEFAULT NULL,
+  `yearOfCompletion` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `mobNo` varchar(30) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `isPermanent` varchar(10) DEFAULT NULL,
+  `contractEndDate` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `staff_user` */
+
+/*Table structure for table `student_user` */
+
+DROP TABLE IF EXISTS `student_user`;
+
+CREATE TABLE `student_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `campus` varchar(30) DEFAULT NULL,
+  `enrollNo` varchar(20) DEFAULT NULL,
+  `firstName` varchar(30) DEFAULT NULL,
+  `lastName` varchar(30) DEFAULT NULL,
+  `fatherName` varchar(30) DEFAULT NULL,
+  `motherName` varchar(30) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `address` varchar(50) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `department` varchar(30) DEFAULT NULL,
+  `courseName` varchar(30) DEFAULT NULL,
+  `yearOfCompletion` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `mobNo` varchar(30) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `hostelNo` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`,`address`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `student_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
